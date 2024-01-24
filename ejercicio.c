f2d5297e3d55e50eb28f02cf0c2496941fff9329
Luis Pineda <luis.pineda.revelo@udla.edu.ec>
2024-01-24 15:57:48 -0500

    El programa recibe los datos correctamente mediante distintas funciones, lo que falta es que estos datos sean escritos en un archivo

--- ejercicio.c
+++ ejercicio.c
@@ -26,18 +26,60 @@ struct producto
         char nombreProducto[25];
         char descrProducto[50];
         int cantidad;
-    };
+    }contenido;
+
+ // Funcion para recibir los productos
+char variosProductos()
+{
+    int otroProducto;
+    puts("Desea ingresar los datos de otro productos: (1 SI | 2 NO)");
+    scanf("%d", &otroProducto);
+    return otroProducto;
+}
+//funcion para obtener los productos
+
+void obtencionDatos(struct  producto *ptr)
+{
+
+    puts("Ingrese el codigo del producto del archivo:\n");
+    scanf("%d", &ptr->codigo);
+    getchar(); // Tengo un problema con el bufer y eso hace que se llene solo el apartado de nombre
+
+    puts("Ingrese el nombre del producto:");
+    gets(ptr->nombreProducto);
+    printf("\n");
+    puts("Ingrese la descripcion del producto:");
+    gets(ptr->descrProducto);
+    printf("\n");
+    puts("Ingrese la cantidad del producto:");
+    scanf("%d", &ptr->cantidad);
+    getchar();
+};
+// funcion que mostraría los productos
+
+void mostrarProductos(struct producto *ptr2, int numeroProducto)
+{
+    for (int i = 0; i < numeroProducto; i++)
+    {
+        puts("Datos del Producto:");
+        printf("Codig: %d\tNombre: %s\tDescripcion %s\tCantidad: %d", ptr2[i].codigo,ptr2[i].nombreProducto,ptr2[i].descrProducto,ptr2[i].cantidad);
+        printf("\n");
+    }
+}
 
 int main(int argc, char const *argv[])
 {
-    // Variables a usar para el archivo
+    // Variables de estructura
+    int numeroProducto = 0;
+    puts("Bienvenido al sistema de registro de productos.");
+    printf("************\n");
+    struct producto MisProductos[100];
+
+    //Variables de archivo
     FILE *ptr;
-
     char archivo[20] = {"MisProducto.txt"};
-    struct producto contenido[500];
 
-
-    // Se va a abrir el archivo
+    //Abrir el archivo
     ptr = fopen(archivo, "w");
     if (ptr)
     {
@@ -48,21 +90,16 @@ int main(int argc, char const *argv[])
         printf("El archivo tuvo un problema al abrirse.\n");
         return 1;
     }
-
-
-    // Se va a empezar a solicitar los productos
-    printf("Ingrese el producto que quiere registra, digite SALIR para dejar de escribir\n");
+    
     do
     {
-        printf("Ingrese el codigo del producto:\n");
-        gets(contenido->codigo);
-        //printf("Ingrese el nombre del producto:\n");
-        //gets(contenido->nombreProducto);
-        fprintf(ptr, "%s\t|", contenido);
-        printf("Escriba la expresion que usted desee y luego retorno para finalizar \n");
-    } while (strcmp(contenido, "SALIR") != 0);
+        obtencionDatos(&MisProductos[numeroProducto]);
+        numeroProducto++;
+    } while (variosProductos() == 1);
 
-    //Cerrar el archivo
+    mostrarProductos(MisProductos, numeroProducto);
+
+     //Cerrar el archivo
     if (fclose(ptr))
     {
         printf("El archivo no se cerro correctamente.\n");
@@ -71,7 +108,23 @@ int main(int argc, char const *argv[])
     {
         printf("El archivo se cerró correctamente.");
     }
-    
+
+ /*
+
+  
+    // Se va a empezar a solicitar los productos
+    printf("Ingrese el producto que quiere registra, digite SALIR para dejar de escribir\n");
+    do
+    {
+        printf("Ingrese el codigo del producto:\n");
+        gets(contenido->codigo);
+        printf("Ingrese el nombre del producto:\n");
+        gets(contenido->nombreProducto);
+        //fprintf(ptr, "%s\t|", contenido);
+        printf("Escriba la expresion que usted desee y luego retorno para finalizar \n");
+    } while (strcmp(contenido, "SALIR") != 0);
+
+    */
     
     
     return 0;
